19f38a0a42b0607a243e551dcfdc30124e5acc59
Mieszko Pugowski <mpugowski@student.agh.edu.pl>
2025-07-20 19:10:53 +0200

    feat: add scraping every game link

--- scraper.py
+++ scraper.py
@@ -25,16 +25,12 @@ BASE_URL = "https://www.fotmob.com/leagues"
 LEAGUE_ID = "38"
 LEAGUE = "bundesliga"
 SUBSITES = ["table","matches","stats","transfers",]
-SEASON = "2025-2026"
-BASE_URL = "https://www.fotmob.com/leagues"
-
 SEASON = "2024-2025"
 
 def pick_subsite(subsite):
     if subsite in SUBSITES:
         subsite_url = "/".join([BASE_URL, LEAGUE_ID, SUBSITES[SUBSITES.index(subsite)],
                                   LEAGUE])
-        url_to_scrape = "?=".join([subsite_url, SEASON])
         url_to_scrape = "?".join([subsite_url, f"season={SEASON}"])
         return url_to_scrape
     else:
@@ -46,4 +42,30 @@ def consent_fotmob():
         EC.element_to_be_clickable((By.CSS_SELECTOR, "button.fc-button.fc-cta-consent.fc-primary-button"))
     )
     consent_button.click()
+
+
+
+def season_games_finder(url):
+    games_list = []
+    driver.get(url)
+    consent_fotmob()
+    for i in range(1):
+        # try:
+        round_i = url +f"&group=by-round&round={i}"
+        driver.get(round_i)
+        time.sleep(1)
+        hrefs = [a.get_attribute("href") for a in
+                 driver.find_elements(By.CSS_SELECTOR, "a.css-1ajdexg-MatchWrapper.e1mxmq6p0")]
+        for link in hrefs:
+            games_list.append(link)
+    driver.quit()
+    if games_list:
+        return games_list
+    else:
+        return "You have exceeded the number of rounds in the league"
+
+bundes_matches = season_games_finder(url=pick_subsite("matches"))
+print(bundes_matches)
+
+
 
